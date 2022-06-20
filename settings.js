1db5d02466a09a53b1b21e4730350bd0ead620ef
RizkyBotz <107808948+RizkyBotz@users.noreply.github.com>
2022-06-20 11:55:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6285876810596','6285876810596','6285876810596'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6285876810596'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6285876810596' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Rizki Muhammad Adam' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'Rizz-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Rizki Muhammad Adam' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qEhdjyw'
+  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwljhw'
   global.myweb = 'https://api-riychdwayne.herokuapp.com'
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© Rizz-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Kinj lu ahh' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
@@ -66,7 +66,7 @@
   // Message
   //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
   global.mess = {
-  success: '_Done._',
+  success: '_✅ Done_',
   admin: '_Fitur Khusus Admin Group!_',
   botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
   owner: '_Fitur Khusus Owner Bot_',
